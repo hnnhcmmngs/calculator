07ae1813653de0d00482ea0b9bac22997f563124
hnnhcmmngs <hannahcummings94@gmail.com>
2024-02-12 20:23:21 -0500

    Add event listeners for number buttons
    When a number is clicked, the appropriate operand needs to be
    updated. The event listeners accomplish this. If there is no
    operator yet (operator is undefined/empty string), then operand1
    is updated. Otherwise, operand2 is updated.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
-let operand1;
-let operator;
-let operand2;
+let operand1 = "";
+let operator = "";
+let operand2 = "";
 
 let displayValue = "";
 
 const display = document.querySelector("#display");
+
+const numberButtons = document.querySelectorAll(".number");
+for (const numberButton of numberButtons) {
+    numberButton.addEventListener("click", () => {
+        if (!operator) {
+            operand1 += numberButton.textContent;
+        } else {
+            operand2 += numberButton.textContent;
+        }
+    })
+}
 
 function operate(operator, a, b) {
     switch(operator) {
@@ -24,6 +35,8 @@ function operate(operator, a, b) {
 }
 
 function updateDisplay(number, append) {
+    // the number may either need to be updated or replaced with a new number
+    // the append argument accounts for this
     if (append === true) {
         display.textContent += number;
     } else {

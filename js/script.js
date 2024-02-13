189aef9ebfaf814d7e0b0c6809175f056bc54c86
hnnhcmmngs <hannahcummings94@gmail.com>
2024-02-12 20:07:35 -0500

    Create updateDisplay function
    As the user clicks a number, the number should appear on the
    display. The updateDisplay function accomplished this. The number
    clicked may either be another digit of the current number or a
    new number. The append argument accounts for this. The new number
    is appended to the display if true and the current text content is
    replaced with the new number if false.

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ let operand1;
 let operator;
 let operand2;
 
+let displayValue = "";
+
+const display = document.querySelector("#display");
+
 function operate(operator, a, b) {
     switch(operator) {
         case "+":
@@ -16,5 +20,13 @@ function operate(operator, a, b) {
         case "/":
             return a / b;
             break;
+    }
+}
+
+function updateDisplay(number, append) {
+    if (append === true) {
+        display.textContent += number;
+    } else {
+        display.textContent = number;
     }
 }
